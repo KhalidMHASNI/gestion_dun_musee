6df4926b803c044184a50948293b3ba08261d1f7
YassineOurara <yassineourara@gmail.com>
2022-12-12 10:03:02 +0100

    app

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kmhas\Documents\GitHub\gestion_dun_musee\myvenv
+set VIRTUAL_ENV=C:\Users\smart asus\OneDrive\Documents\GitHub\gestion_dun_musee\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
