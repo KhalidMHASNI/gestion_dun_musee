dc5c0201f8b2aa59264c677951f2e8e23f0c8ce6
YassineOurara <yassineourara@gmail.com>
2022-12-24 23:46:09 +0100

    !!...

--- gestmuseeAPP/urls.py
+++ gestmuseeAPP/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home, name="home")
+    path('',views.home, name="home"),
+    path('Signinup/',views.SigninupPage, name="Signinup")
     
 ]
--- gestmuseeAPP/views.py
+++ gestmuseeAPP/views.py
@@ -22,3 +22,39 @@ from django.template.loader import get_template
 # Create your views here.
 def home(request):
 	return render(request,'home.html')
+
+#Signup  page
+def SigninupPage(request):
+	if request.method=='POST' and 'btnform2' in request.POST: 
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		email = request.POST['email']
+		password1 = request.POST['password']
+		password2 = request.POST['cpassword']
+
+
+		if password1 == password2 :
+			if User.objects.filter(email=email).exists():
+				messages.warning(request,'Email taken')
+				return redirect('Signinup')
+			else :
+				myuser = User.objects.create_user(username=email, email=email,first_name=first_name,last_name=last_name ,password=password1)
+				myuser.save()
+				messages.success(request, "Votre compte est crée .")
+				return redirect('Signinup')
+		else:
+			messages.info(request,'password not matching')
+			return redirect('Signinup')
+	if request.method== 'POST' and 'btnform1' in request.POST:
+		email = request.POST['username']
+		password = request.POST['password']
+		user = auth.authenticate(username=email,password=password) 
+
+		if user is not None:
+			auth.login(request,user)
+			return redirect('formation')
+		else :
+			messages.error(request,"Password missmatchs")
+			return redirect('Signinup')
+	else: 
+		return render(request, 'Signinup.html')
