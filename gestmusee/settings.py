37a455159dff69addecc2f8ecb450398b249945b
YassineOurara <yassineourara@gmail.com>
2023-01-20 18:10:30 +0100

    static load

--- gestmusee/settings.py
+++ gestmusee/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gestmusee.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'darjamai',
+        # 'NAME': 'darjamai',
         'USER': 'darjamaiAdmin',
         'PASSWORD': 'darjamaiAdmin',
         'HOST': 'localhost',
