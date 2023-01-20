8a808ee46bc92b8721c45f8679399a8942f6622b
YassineOurara <yassineourara@gmail.com>
2023-01-21 00:09:17 +0100

    view oeuvre  ..

--- gestmuseeAPP/urls.py
+++ gestmuseeAPP/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
-    path('Signinup/',views.SigninupPage, name="Signinup")
+    path('Signinup/',views.SigninupPage, name="Signinup"),
+    path('oeuvres/',views.oeuvres, name="oeuvres")
+
 ]
